f96eed50e08eeee0274d8206ab15fa0ebd918518
Jean-Alain Ré <jre@hi-media.com>
2014-01-30 18:04:18 +0100

    add boostrasp & design form and list

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,9 +1,12 @@
 'use strict';
 
 angular.module('rchSeanceApp')
-  .controller('MainCtrl', function ($scope, Seances) {
+  .controller('MainCtrl', function ($scope, $location, Seances) {
     $scope.seances = Seances.getAll();
     $scope.delete = function(index){
-        alert(index);
+        //TODO
     }
+    $scope.go = function (path) {
+        console.dir($location.path(path));
+    };
   });

76ce54cc7450967f87f18950f3785eb63573f3dd
JA <jeanlain.re@gmail.com>
2014-02-01 01:06:55 +0100

    correction on form

--- app/scripts/controllers/new.js
+++ app/scripts/controllers/new.js
@@ -1,7 +1,12 @@
 'use strict';
 
 angular.module('rchSeanceApp')
-    .controller('CreateCtrl', function($scope, $location, Seances) {
+    .controller('CreateCtrl', function($scope, $location, $filter, Seances) {
+        $scope.seance = {
+            date: $filter("date")(Date.now(), 'yyyy-MM-dd'),
+            start: '20:00',
+            end: '22:00'
+        };
         $scope.save = function() {
             Seances.save({}, $scope.seance);
             $location.path('/');
